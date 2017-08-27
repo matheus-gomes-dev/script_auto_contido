fc62d6d1f743b277641c927a35641b1573767744
matheus-lima92 <lima.eca@gmail.com>
2017-08-27 13:10:09 -0300

    testando comunicação com API

--- main.js
+++ main.js
@@ -4,5 +4,31 @@ console.log(aux)
 
 
 var htmlTemplate =  '<button>TESTE</button>'
+var APIurl = 'https://mid-homolog.totvs.com:4007/api/v2';
+
+//===CATALOG V2 API===
+function catalogV2API(method, endpoint, data, cb){
+    $.ajax({
+        "url": APIurl + endpoint,
+        "type": method,
+        "contentType": "application/json",
+        "dataType": "json",
+        "data": JSON.stringify(data),
+        "Accept": "application/json",
+        "success": function (response) {
+            cb(response);
+        },
+        "error": function (jqXHR, textStatus, errorThrown) {
+            console.log(textStatus, errorThrown);
+            cb("Erro na operação!")
+        }
+    });
+}
+//======
+
+catalogV2API('GET', '/campos', '', function(response){
+	console.log(response);
+});
 
 document.body.innerHTML +=htmlTemplate;
+
